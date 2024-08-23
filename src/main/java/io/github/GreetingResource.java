d4e4d5658ad2fc4009a877890e02f59c5fe7fb74
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2024-08-23 14:57:14 +0200

    initial commit

---
+++ src/main/java/io/github/GreetingResource.java
@@ -0,0 +1,17 @@
+package io.github;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response hello() {
+        return Response.ok("Hello from Quarkus REST").build();
+    }
+}
